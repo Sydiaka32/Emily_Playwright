188422dc17b6bd261b5c62fbceb871b28758c16e
Sydiaka32 <max.sydiaka@gmail.com>
2025-02-03 22:34:03 +0200

    Updated edit actions with waiting

--- tests/test_02edit_draft.py
+++ tests/test_02edit_draft.py
@@ -6,47 +6,30 @@ from pages.auction_page import AuctionPage
 from locators.my_auctions_locators import MyAuctionsLocators
 
 
-def test_edit_draft():
-    with sync_playwright() as playwright:
-        # Launch the browser and create a new page
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
+def test_edit_draft(navigate_to_my_auctions):
+    page = navigate_to_my_auctions  # The browser object passed by the fixture
+    navigation_page = NavigationPage(page)
+    my_auctions_page = MyAuctionsPage(page)
+    auction_page = AuctionPage(page)
+    my_auctions_locators = MyAuctionsLocators()
 
-        # Initialize the LoginPage, NavigationPage, and MyAuctionsPage
-        login_page = LoginPage(page)
-        navigation_page = NavigationPage(page)
-        my_auctions_page = MyAuctionsPage(page)
-        auction_page = AuctionPage(page)
-        my_auctions_locators = MyAuctionsLocators()
+    # Step 2: Navigate to "My Auctions"
+    navigation_page.navigate_to_my_auctions()
 
-        # Step 1: Log in to the application
-        login_page.login()
+    # Step 6.1: Close telegram popup
+    navigation_page.close_telegram_popup()
 
-        # Step 2: Navigate to "My Auctions"
-        navigation_page.navigate_to_my_auctions()
+    page.wait_for_timeout(2000)
 
-        # Step 6.1: Close telegram popup
-        navigation_page.close_telegram_popup()
+    # Step 13: Go to edit mode of draft and edit
+    my_auctions_page.edit_mode()
+    auction_page.edit_title()
+    page.wait_for_timeout(1000)
+    auction_page.save_changes()
 
-        page.wait_for_timeout(3000)
+    # Handle popup and updating time
+    page.wait_for_timeout(2000)
 
-        # Step 13: Go to edit mode of draft and edit
-        my_auctions_page.edit_mode()
-        auction_page.edit_title()
-        auction_page.save_changes()
-
-        # Handle popup and updating time
-        page.wait_for_timeout(3000)
-
-        # Step 14: Compare edited title
-        assert my_auctions_page.get_card_title() == "AuctionEditedForDeletion", f"Expected title to be 'Auction', but got '{MyAuctionsPage.get_card_title()}'"
-        page.locator(my_auctions_locators.AUCTION_CARD).screenshot(path="screenshotedited.png")
-
-        # Close the browser after the test
-        browser.close()
-
-
-# Run the test
-if __name__ == "__main__":
-    test_edit_draft()
+    # Step 14: Compare edited title
+    assert my_auctions_page.get_card_title() == "AuctionEditedForDeletion", \
+        f"Expected title to be 'Auction', but got '{MyAuctionsPage.get_card_title()}'"
