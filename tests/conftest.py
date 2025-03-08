00f6c10f3005d756ec40cd1c3128682befe53cff
Sydiaka32 <max.sydiaka@gmail.com>
2025-03-08 11:27:01 +0200

    removed unnecessary fixture for taking step with screenshot

--- tests/conftest.py
+++ tests/conftest.py
@@ -53,16 +53,6 @@ def get_auction_id(navigate_to_my_auctions):
 
 
 @pytest.fixture
-def allure_step_with_screenshot(sync_page):
-    def _step(name, action):
-        with allure.step(name):
-            result = action()  # Run the provided action (function)
-            allure.attach(sync_page.screenshot(), name=name, attachment_type=allure.attachment_type.PNG)
-            return result  # Return the result of the action if needed
-    return _step
-
-
-@pytest.fixture
 def allure_step(sync_page):
     def _step(name, action, take_screenshot=True):
         with allure.step(name):
