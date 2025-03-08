2b7beaaae0d9f7028af89ad090508761bbef8961
Sydiaka32 <max.sydiaka@gmail.com>
2025-03-08 11:46:57 +0200

    added allure steps to publish draft test

--- tests/test_04publish_draft.py
+++ tests/test_04publish_draft.py
@@ -2,26 +2,28 @@ from pages.my_auctions_page import MyAuctionsPage
 from pages.navigation_page import NavigationPage
 
 
-def test_publish_draft(navigate_to_my_auctions):
+def test_publish_draft(navigate_to_my_auctions, allure_step):
     page = navigate_to_my_auctions  # The browser object passed by the fixture
     my_auctions_page = MyAuctionsPage(page)
     navigation_page = NavigationPage(page)
 
     # Step 1: Publish draft
-    my_auctions_page.publish_option()
+    allure_step("Publish draft", my_auctions_page.publish_option(), take_screenshot=True)
 
     # Handle updating time
     page.wait_for_timeout(1500)
 
     # Refresh the current page
-    page.reload()
+    allure_step("Refresh current page", page.reload(), take_screenshot=False)
 
     # Handle popup and updating time
     page.wait_for_timeout(3000)
     navigation_page.close_notification_if_exists()
 
+    card_status = allure_step("Get auction status", my_auctions_page.get_card_published_status(), take_screenshot=True)
+
     # Step 2: Compare edited title
-    assert my_auctions_page.get_card_published_status() == "Прийняття заяв на участь", \
+    assert card_status == "Прийняття заяв на участь", \
         (f"Expected title to be 'Прийняття заяв на участь', "
-         f"but got '{my_auctions_page.get_card_published_status()}'")
+         f"but got '{card_status}'")
 
