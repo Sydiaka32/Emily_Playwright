dd08dea18c137b2f38749086c50011bc3b851ab2
Sydiaka32 <max.sydiaka@gmail.com>
2025-05-19 21:46:18 +0300

    Updated test 3 with api fixture

--- tests/test_03delete_draft.py
+++ tests/test_03delete_draft.py
@@ -2,15 +2,20 @@ import pytest
 
 from config.settings import ConfigParser
 from pages.user.my_auctions_page import MyAuctionsPage
+from pages.user.navigation_page import NavigationPage
+
 
 @pytest.mark.parametrize('login', ['t1'], indirect=True)
-def test_delete_draft(navigate_to_my_auctions, get_auction_id, allure_step, login):
-    page = navigate_to_my_auctions  # The browser object passed by the fixture
+def test_delete_draft(created_auction, allure_step, login):
+    page = login
     my_auctions_page = MyAuctionsPage(page)
     config_parser = ConfigParser()
+    navigation = NavigationPage(page)
 
-    # Step 1: Retrieve the original auction ID using the fixture
-    auction_id = allure_step("Retrieve auction id", lambda: get_auction_id(), take_screenshot=False)
+    auction_id = created_auction
+
+    # Step 1: Navigate to my auctions
+    allure_step("Navigate to my auctions", lambda: navigation.navigate_to_my_auctions(), take_screenshot=False)
 
     # Step 2: Delete draft
     allure_step("Delete draft", lambda: my_auctions_page.delete_option(), take_screenshot=False)
