dbdcdf1774ca743a3986b891a5e67ac692244575
Sydiaka32 <max.sydiaka@gmail.com>
2025-06-02 21:33:46 +0300

    Updating case 8 with new fixtures

--- tests/test_08is_perishable.py
+++ tests/test_08is_perishable.py
@@ -5,17 +5,18 @@ from pages.user.auction_page import AuctionPage
 from pages.user.my_auctions_page import MyAuctionsPage
 
 @pytest.mark.parametrize('login', ['t1'], indirect=True)
-def test_is_perishable(navigate_to_my_auctions, get_auction_id, allure_step, capture_api_values, login):
-    page = navigate_to_my_auctions  # The browser object passed by the fixture
+def test_is_perishable(created_auction, get_auction_id, allure_step, capture_api_values, login):
+    page = login
     my_auctions_page = MyAuctionsPage(page)
     auction_page = AuctionPage(page)
     navigation_page = NavigationPage(page)
 
     # Auction creation steps
+    allure_step("Navigate to my auctions", lambda: navigation_page.navigate_to_my_auctions(), take_screenshot=False)
     allure_step("Navigate to 'New Auction' page", my_auctions_page.navigate_to_new_auction, take_screenshot=False)
     allure_step("Select organiser", auction_page.select_organiser, take_screenshot=False)
     allure_step("Select procedure", auction_page.select_procedure, take_screenshot=False)
-    allure_step("Close Telegram popup", navigation_page.close_telegram_popup, take_screenshot=False)
+    # allure_step("Close Telegram popup", navigation_page.close_telegram_popup, take_screenshot=False)
     allure_step("Fill in Basic info block", auction_page.fill_basic_info_block, take_screenshot=True)
     allure_step("Fill in Details block", auction_page.fill_detail_lot_description_block, take_screenshot=True)
     allure_step("Select classifier", lambda: auction_page.select_perishable_classifier(), take_screenshot=True)
